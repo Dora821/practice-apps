6f4763b27166188840bda650d45945ef42e36e42
Dora Xia <duruoxia1992@gmail.com>
2022-03-23 16:35:22 -0700

    server is connected to database and can make queries

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,8 +1,41 @@
-const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017.glossary');
-
+const mongoose = require('mongoose');
 // 1. Use mongoose to establish a connection to MongoDB
-const
+mongoose.connect('mongodb://localhost:27017/Glossary');
 // 2. Set up any schema and models needed by the app
+let GlossarySchema = mongoose.Schema({
+  word: {type: String, unique: true},
+  definition: String
+});
 // 3. Export the models
+const Glossary = mongoose.model('Glossary', GlossarySchema);
 // 4. Import the models into any modules that need them
+
+// module.exports = {
+const getAll = ()=> {
+  return Glossary.find({});
+};
+const getOne = (key) => {
+  console.log('db function runnning', key);
+  return Glossary.findOne({word:key}).exec();
+};
+const save = (obj)=> {
+  console.log('data in obj', obj);
+  return Glossary.save(obj);
+};
+
+const update = (filter, update) => {
+  console.log('filter', filter);
+  console.log('update', update);
+  return Glossary.findOneAndUpdate(filter, update, {upsert:true});
+};
+
+const delEntry = (obj) => {
+  return Glossary.deleteOne(obj);
+};
+
+// };
+module.exports.getAll = getAll;
+module.exports.getOne = getOne;
+module.exports.save = save;
+module.exports.update = update;
+module.exports.delEntry = delEntry;
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,59 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
+const db = require('./db.js');
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.json());
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+app.post('/addNote', (req, res)=> {
+  let input = req.body;
+  let filter = {'word': req.body.word};
+  let updates = {'definition': req.body.definition};
+  console.log('input in server', input);
+  console.log(typeof db.update);
+  db.update(filter, updates)
+    .then(()=> {
+      db.getAll().
+        then((data)=>res.send(data))
+        .catch((err)=>console.log('error occurs when getting info from DB'));
+    })
+    .catch(error=> console.error('error occured when saving data into DB', error));
+  // -----------------------
+  // db.getAll()
+  //   .then((data)=>res.send(data))
+  //   .catch((err)=>console.log('err in post server', err));
+// ---------------------------
+  // console.log('requet body', input);
+//   db.save(input)
+//     .then(()=> {
+//       db.getAll().
+//         then((data)=>res.send(data))
+//         .catch((err)=>console.log('error occurs when getting info from DB'));
+//     })
+//     .catch(error=> console.error('error occured when saving data into DB'));
+});
+
+
+app.get('/search', (req, res)=> {
+  let keyWord = req.body.word;
+  console.log('keyword searched', keyWord);
+  db.getOne(keyWord)
+    .then((data)=>res.send(data))
+    .catch(error => console.log('error occurs when searching in DB'));
+});
+
+
+app.delete('/delete', (req, res)=>{
+  let delKey = {'word':req.body.word};
+  db.delEntry(delKey)
+    .then(()=>res.send('success'))
+    .catch(err=>res.send(err));
+
+});
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
